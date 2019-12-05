8f5e56786d1b78f4d022e3f92a0c45e556fbf7fd
kalebryler <34117823+kalebryler@users.noreply.github.com>
2019-12-05 03:07:12 -0500

    Add files via upload

--- learning_model.py
+++ learning_model.py
@@ -41,6 +41,7 @@ def read_file(team_name):
 
 	data = pd.read_csv(file_name)
 	data.set_index('Date',inplace=True)
+	data = data.sort_values('Date')
 	data = data.fillna(0)
 
 	return data
@@ -99,25 +100,22 @@ def output_vars():
 
 	return outs
 
-def get_inputs_outputs(df):
+def get_inputs_outputs(df,outcome):
 	var_list = output_vars()
 
 	d = {}
 
-	for outcome in ['Win','Over','F5_Over','Cover']:
-		d[outcome] = {}
+	inputs = df[var_list]
+	# outputs = df[['True_'+outcome]]
+	outputs = df[[outcome]]
+	try:
+		ml = df[[outcome + "_ML"]]
+	except:
+		ml = df[["ML"]]
 
-		inputs = df[var_list]
-		outputs = df[['True_'+outcome]]
-		# outputs = df[[outcome]]
-		try:
-			ml = df[[outcome + "_ML"]]
-		except:
-			ml = df[["ML"]]
-
-		d[outcome]['Inputs'] = inputs.to_numpy()
-		d[outcome]['Outputs'] = outputs.to_numpy()
-		d[outcome]['Payout'] = ml.to_numpy()
+	d['Inputs'] = inputs.to_numpy()
+	d['Outputs'] = outputs.to_numpy()
+	d['Payout'] = ml.to_numpy()
 
 	return d
 
@@ -144,13 +142,13 @@ def model_team(team_name):
 	for outcome in ['Win','Over','F5_Over','Cover']:
 		d[outcome] = {}
 
-		d[outcome]['NeuralNet'] = {}
+		# d[outcome]['NeuralNet'] = {}
 		net = NeuralNet(var_dict[outcome]['Inputs'],var_dict[outcome]['Outputs'],var_dict[outcome]['Payout'])
 		net.model()
 		# d[outcome]['NeuralNet']['Results'] = net.results
-		d[outcome]['NeuralNet']['Error'] = '{:.2%}'.format(net.error)
-		d[outcome]['NeuralNet']['Accuracy'] = '{:.2%}'.format(net.accuracy)
-		d[outcome]['NeuralNet']['Profit'] = '{:.2%}'.format(net.profit)
+		d[outcome]['Error'] = '{:.2%}'.format(net.error)
+		d[outcome]['Accuracy'] = '{:.2%}'.format(net.accuracy)
+		d[outcome]['Profit'] = '{:.2%}'.format(net.profit)
 
 		# d[outcome]['SVM'] = {}
 		# svm = SVM(var_dict[outcome]['Inputs'],var_dict[outcome]['Outputs'],var_dict[outcome]['Payout'])
@@ -176,33 +174,37 @@ def model_all():
 
 class NeuralNet:
 	def __init__(self,x,y,ml):
-		self.x = x
-		self.y = y
-		self.ml = ml
-
-		self.input_size = self.x.shape[1]
+		self.x = x[:-1]
+		self.y = y[:-1]
+		self.ml = ml[:-1]
 
 		self.x_train,self.x_test,self.y_train,self.y_test,self.ml_train,self.ml_test = train_test_split(self.x,self.y,self.ml,test_size = 0.25)
 
-	def model(self):
+		self.x_game = x[-1]
+		self.y_game = y[-1]
+		self.ml_game = ml[-1]
+
 		scaler = StandardScaler()
 		scaler.fit(self.x_train)
 		self.x_train = scaler.transform(self.x_train)
 		self.x_test = scaler.transform(self.x_test)
+		self.x_game = scaler.transform([self.x_game])
 
 		encoder = preprocessing.LabelEncoder()
 		encoder.fit(self.y_train.ravel())
 		self.y_train = encoder.transform(self.y_train.ravel())
 		self.y_test = encoder.transform(self.y_test.ravel())
+		self.y_game = encoder.transform(self.y_game.ravel())
 
+		self.input_size = self.x.shape[1]
 		self.output_size = len(set(self.y_train))
 
+	def model(self):
 		net = MLPClassifier(hidden_layer_sizes=(int(round_up(self.input_size,100)),int(round_up(self.input_size/2,100)),int(round_up(self.input_size/5,100))), max_iter=10000)
-		fit = net.fit(self.x_train, self.y_train.ravel())
-		predictions = fit.predict(self.x_test)
+		self.fit = net.fit(self.x_train, self.y_train.ravel())
+		predictions = self.fit.predict(self.x_test)
 
 		# self.metrics = classification_report(self.y_test,predictions)
-
 		self.results = pd.DataFrame([predictions,self.y_test,self.ml_test.ravel()]).T
 
 		self.results.columns = ['Predicted','Actual','ML']
@@ -215,47 +217,61 @@ class NeuralNet:
 
 		self.error = self.results['Error'].mean()
 		self.accuracy = self.results['Success'].mean()
-		self.profit = self.results['Payout'].mean()
+		self.pct_return = self.results['Payout'].mean()
+		self.profit = self.results['Payout'].sum()
+
+	def predict_game(self):
+		predictions = self.fit.predict(self.x_game)
+
+		self.game_results = pd.DataFrame([predictions,self.y_game,self.ml_game.ravel(),self.accuracy.ravel()]).T
+		self.game_results.columns = ['Predicted','Actual','ML','Test_Accuracy']
+		self.game_results['Error'] = abs(self.game_results['Predicted']-self.game_results['Actual'])
+		self.game_results['Success'] = np.select([round_up(self.game_results['Predicted']-0.05,0.5)==self.game_results['Actual'],round_down(self.game_results['Predicted']+0.05,0.5)==self.game_results['Actual']],[1,1],0)
+		self.game_results['Payout'] = np.select([self.game_results['Test_Accuracy']<0.2,self.game_results['Success']==1],[0,self.game_results['ML']],-1)
 
 
-class SVM:
-	def __init__(self,x,y,ml):
-		self.x = x
-		self.y = y
-		self.ml = ml
+def model_game(team_name,model_name,outcome,date,var_dict=None):
+	team_df = read_file(team_name)
+	team_df = team_df[:date]
 
-		self.x_train,self.x_test,self.y_train,self.y_test,self.ml_train,self.ml_test = train_test_split(self.x,self.y,self.ml,test_size = 0.25)
+	if var_dict == None:
+		var_dict = get_inputs_outputs(team_df,outcome)
 
-	def model(self):
-		scaler = StandardScaler()
-		scaler.fit(self.x_train)
-		self.x_train = scaler.transform(self.x_train)
-		self.x_test = scaler.transform(self.x_test)
+	model = model_name(var_dict['Inputs'],var_dict['Outputs'],var_dict['Payout'])
+	model.model()
+	model.predict_game()
 
-		encoder = preprocessing.LabelEncoder()
-		encoder.fit(self.y_train.ravel())
-		self.y_train = encoder.transform(self.y_train.ravel())
-		self.y_test = encoder.transform(self.y_test.ravel())
+	return model.game_results
 
-		net = svm.SVR(gamma='auto')
-		fit = net.fit(self.x_train, self.y_train.ravel())
-		predictions = fit.predict(self.x_test)
+def model_season(team_name,model_name,outcome):
+	d = []
 
-		self.results = pd.DataFrame([predictions,self.y_test,self.ml_test.ravel()]).T
+	team_df = read_file(team_name)
+	dates = team_df.index
 
-		self.results.columns = ['Predicted','Actual','ML']
-		self.results['Error'] = abs(self.results['Predicted']-self.results['Actual'])
-		self.results['Success'] = np.select([round_up(self.results['Predicted']-0.05,0.5)==self.results['Actual'],round_down(self.results['Predicted']+0.05,0.5)==self.results['Actual']],[1,1],0)
-		self.results['Payout'] = np.select([self.results['Success']==1,self.results['Success']==0],[self.results['ML'],-1],0)
+	for date in dates[30:]:
+		game = model_game(team_name,model_name,outcome,date)
+		d.append(game)
+	
+	df = pd.concat(d,axis=0,sort=False)
+	df.index = [date for date in dates[30:]]
+	df['Bet'] = np.where(df['Payout']!=0,1,0)
 
-		self.error = self.results['Error'].mean()
-		self.accuracy = self.results['Success'].mean()
-		self.profit = self.results['Payout'].mean()
+	print(df)
+	print('Error: ' + str(df['Error'].mean()))
+	print('Accuracy: ' + str(df['Success'].mean()))
+	print('Avg. Return: ' + str(df['Payout'].mean()))
+	print('Total Profit: ' + str(df['Payout'].sum()))
+	print('Num. Bets: ' + str(df['Bet'].sum()))
+
+	return df
+
 
 ##########
+model_season("Texas Rangers",NeuralNet,"Win")
 # print_model("All Teams",NeuralNet,"Win")
 
-a = model_all()
-print(a)
+# a = model_all()
+# print(json.dumps(a,indent=4))
 
 
